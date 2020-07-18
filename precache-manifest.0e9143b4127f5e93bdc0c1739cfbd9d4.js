21141d2547222c7fb252941ce88088d10a43ae3f
Yeger <ohad1995@gmail.com>
2020-07-19 01:16:39 +0300

    Updates

---
+++ precache-manifest.0e9143b4127f5e93bdc0c1739cfbd9d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb13da71732497a5fcbf03293931fe5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10167474cfbed80acea",
+    "url": "/static/js/2.50a6867e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.50a6867e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d078e1fc1cb51ee1699f",
+    "url": "/static/js/main.ccd46d4f.chunk.js"
+  },
+  {
+    "revision": "058bfbcd3bb70b11203a",
+    "url": "/static/js/runtime-main.29e55df0.js"
+  }
+]);
